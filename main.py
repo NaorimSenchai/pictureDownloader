c89c35d5a37388a528ab32ede88c35ba1d203509
Romain <romain.c@institutsolacroup.com>
2022-05-04 09:25:06 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#coding:utf-8
+
+import os
+import urllib.request
+import getpass
+import shutil
+
+urllib.request.urlretrieve("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/070226_Shy_mollymawk_off_Kaikoura_2.jpg/290px-070226_Shy_mollymawk_off_Kaikoura_2.jpg", "Albatros.png")
